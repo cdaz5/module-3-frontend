55fd13bf53f9659ecbe78eaf121f00915cb9edb6
github username <github email address>
2017-07-11 18:44:46 -0400

    working api db seeded

--- adapter.js
+++ adapter.js
@@ -1,3 +1,6 @@
+window.onload = function() {
+  // getData()
+}
 let apple = []
 let amazon = []
 let google = []
@@ -5,45 +8,46 @@ let microsoft = []
 let facebook = []
 
 function getData() {
-    fetch('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20160101&date.lt=20170710&ticker=MSFT,FB,APPL,GOOGL,AMZN&api_key=-uF_NbkA7_3nmTJLmr4t', {
+    fetch('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20160101&date.lt=20170710&ticker=MSFT,FB,AAPL,GOOGL,AMZN&api_key=-uF_NbkA7_3nmTJLmr4t', {
     method: 'GET',
 })
 .then(response => response.json())
-.then(json => separateData(json))
+.then(json => separateData(json, postData))
+// .then(postData(amazon))
+// .then(postData(apple))
+// .then(postData(facebook))
+// .then(postData(microsoft))
+// .then(postData(google))
+
 }
 
-function separateData(json) {
+function separateData(json, callback) {
     json.datatable.data.forEach(arr => {
         if (arr[0] === 'AMZN') {
-            amazon.push( {name: 'Amazon', ticker: arr[0], date: arr[1], price: arr[5]} )
+            amazon.push( {stock: {name: 'Amazon', ticker: arr[0], date: arr[1], price: arr[5]} })
         } else if (arr[0] === 'MSFT') {
-            microsoft.push( {name: 'Microsoft', ticker: arr[0], date: arr[1], price: arr[5]} )
-        } else if (arr[0] === 'APPL') {
-            apple.push( {name: 'Apple', ticker: arr[0], date: arr[1], price: arr[5]} )
+            microsoft.push( {stock: {name: 'Microsoft', ticker: arr[0], date: arr[1], price: arr[5]} })
+        } else if (arr[0] === 'AAPL') {
+            apple.push( {stock: {name: 'Apple', ticker: arr[0], date: arr[1], price: arr[5]} })
         } else if (arr[0] === 'GOOGL') {
-            google.push( {name: 'Google', ticker: arr[0], date: arr[1], price: arr[5]} )
+            google.push( {stock: {name: 'Google', ticker: arr[0], date: arr[1], price: arr[5]}} )
         } else if (arr[0] === 'FB') {
-            facebook.push( {name: 'Facebook', ticker: arr[0], date: arr[1], price: arr[5]} )
-        } 
-    }) 
+            facebook.push( {stock: {name: 'Facebook', ticker: arr[0], date: arr[1], price: arr[5]} })
+        }
+    })
+    callback(apple)
+    callback(amazon)
+    callback(google)
+    callback(facebook)
+    callback(microsoft)
 }
-getData()
 
 function postData(data) {
     fetch('http://localhost:3000/stocks', {
-        method: 'POST', 
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data),
     })
-    .catch(error => console.log("Error: " + error) )
+
 }
-
-
-
-
-
-
-
-
-
-
-
+//
