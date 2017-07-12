7dbe64789c3d8d9f0da772dbff6915e26247cf72
sochetanie <allagorik2901@gmail.com>
2017-07-12 18:20:20 -0400

    done something

--- adapter.js
+++ adapter.js
@@ -1,6 +1,12 @@
-window.onload = function() {
-  // getData()
-}
+// window.onload = function() {
+//   // getData()
+//   createUser()
+// }
+
+$(document).ready(function(){
+  createUser()
+
+})
 let apple = []
 let amazon = []
 let google = []
@@ -13,11 +19,6 @@ function getData() {
 })
 .then(response => response.json())
 .then(json => separateData(json, postData))
-// .then(postData(amazon))
-// .then(postData(apple))
-// .then(postData(facebook))
-// .then(postData(microsoft))
-// .then(postData(google))
 
 }
 
@@ -48,6 +49,142 @@ function postData(data) {
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data),
     })
+  }
+
+function createUser() {
+  debugger
+  $('#signup').submit(function(event) {
+  event.preventDefault();
+  let values = $(this).serialize()
+  let posting = $.post('http://localhost:3000/users', values)
+      posting.done(function(data) {
+        $('#signup').toggle()
+        $('#username').text(`Hi ${data.name}`)
+        let portfolioForm = new MyForm()
+        $('#portfolioForm').append(portfolioForm.render())
+        ourAPI()
+        grabPortfolio()
+      })
+  })
+}
+
+function grabPortfolio() {
+  debugger
+  $('#portfolioForm').on('submit', '#portForm', function(event) {
+    event.preventDefault();
+    let selections = document.querySelectorAll('.portfolio')
+    let chosen = []
+    selections.forEach((company) => {
+      if (company.checked) {
+        chosen.push(company.value)
+      }
+    })
+    debugger
+  })
+}
+
+class MyForm {
+  constructor() {}
+  render() {
+    return (
+    `<form id="portForm">
+      <div>
+        <input type="checkbox" class="portfolio" name="subscribe" value="apple">
+        <label for="subscribeNews">Apple</label>
+         <input type="checkbox" class="portfolio" name="subscribe" value="amazon">
+        <label for="subscribeNews">Amazon</label>
+          <input type="checkbox" class="portfolio" name="subscribe" value="google">
+        <label for="subscribeNews">Google</label>
+         <input type="checkbox" class="portfolio" name="subscribe" value="facebook">
+        <label for="subscribeNews">Facebook</label>
+        <input type="checkbox" class="portfolio" name="subscribe" value="microsoft">
+        <label for="subscribeNews">Microsoft</label>
+      </div>
+      <div>
+        <button type="submit">Create Portfolio</button>
+      </div>
+    </form>`)
+  }
+}
+
+let chartArray=[]
+function filterData(data, companyName){
+  let companyArray=[];
+
+   let twentySixteen = data.filter((stock)=>{
+    return stock.name ===companyName
+  }).filter((stock)=>{
+    return stock.date.substring(0,4) ==='2016'
+  }).map((el)=>{
+    return el.price
+  }) 
+
+  let twentySeventeen = data.filter((stock)=>{
+    return stock.name ===companyName
+  }).filter((stock)=>{
+    return stock.date.substring(0,4) ==='2017'
+  }).map((el)=>{
+    return el.price
+  }) 
+
+  twentySixteen.unshift(companyName + ' 2016')
+  twentySeventeen.unshift(companyName + ' 2017')
+  companyArray.push(twentySixteen)
+  companyArray.push(twentySeventeen)
+  chartArray.push(companyArray)
+}
+
+function ourAPI(){
+debugger
+$.get('http://localhost:3000/stocks',function(data){
+  console.log(data)
+  let amazon = filterData(data, 'Amazon')
+  let google = filterData(data, 'Google')
+  let microsoft = filterData(data, 'Microsoft')
+  let facebook = filterData(data, 'Facebook')
+  let apple = filterData(data, 'Apple')
+ 
+  // chart(chartArray)
+  })
+  
+}
+
+function chart(chartArray){
+  var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        chartArray[0][0],
+        chartArray[0][1],
+        chartArray[1][0],
+        chartArray[1][1],
+        chartArray[2][0],
+        chartArray[2][1],
+        chartArray[3][0],
+        chartArray[3][1], 
+        chartArray[4][0],
+        chartArray[4][1],        
+      ]
+    },
+    axis: {
+      x: {
+        label: {
+          text: 'Months',
+          position: 'outer-center'
+        }
+      },
+      y: {
+        label: {
+          text: 'Prices',
+          position: 'outer-middle'
+        }
+      }
+
+    }
+
+});
 
 }
-//
+
+   
+
