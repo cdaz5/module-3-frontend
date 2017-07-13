898a2f27f0c090a1024d3d12168852e6bbe2f05f
sochetanie <allagorik2901@gmail.com>
2017-07-13 15:50:02 -0400

    add changes to css

--- adapter.js
+++ adapter.js
@@ -1,7 +1,6 @@
 // window.onload = function() {
-//   // getData()
-//   createUser()
-
+//   getData()
+//   // createUser()
 // }
 
 $(document).ready(function(){
@@ -18,9 +17,12 @@ let amazon = []
 let google = []
 let microsoft = []
 let facebook = []
+let nvidia = []
+let ibm = []
+let ebay = []
 
 function getData() {
-    fetch('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20160101&date.lt=20170710&ticker=MSFT,FB,AAPL,GOOGL,AMZN&api_key=-uF_NbkA7_3nmTJLmr4t', {
+    fetch('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20160101&date.lt=20170710&ticker=MSFT,FB,AAPL,GOOGL,AMZN,NVDA,IBM,EBAY&api_key=-uF_NbkA7_3nmTJLmr4t', {
     method: 'GET',
 })
 .then(response => response.json())
@@ -40,6 +42,12 @@ function separateData(json, callback) {
             google.push( {stock: {name: 'Google', ticker: arr[0], date: arr[1], price: arr[5]}} )
         } else if (arr[0] === 'FB') {
             facebook.push( {stock: {name: 'Facebook', ticker: arr[0], date: arr[1], price: arr[5]} })
+        } else if (arr[0] === 'NVDA') {
+            nvidia.push( {stock: {name: 'NVIDIA', ticker: arr[0], date: arr[1], price: arr[5]} })
+        } else if (arr[0] === 'IBM') {
+            ibm.push( {stock: {name: 'IBM', ticker: arr[0], date: arr[1], price: arr[5]} })
+        } else if (arr[0] === 'EBAY') {
+            ebay.push( {stock: {name: 'eBay', ticker: arr[0], date: arr[1], price: arr[5]} })
         }
     })
     callback(apple)
@@ -47,9 +55,14 @@ function separateData(json, callback) {
     callback(google)
     callback(facebook)
     callback(microsoft)
+    callback(nvidia)
+    callback(ibm)
+    callback(ebay)
 }
 
 function postData(data) {
+  // debugger
+  console.log(data)
     fetch('http://localhost:3000/stocks', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -73,38 +86,15 @@ function createUser() {
   })
 }
 
-function startPlaybackGunit() {
-  return document.querySelector('#gunit').play();
-}
-
-function startPlaybackSweedish() {
-  return document.querySelector('#sweedish').play();
-}
-
-
 function grabPortfolio() {
   $('#chart').show()
   $('#portfolioForm').on('click', '#buttn', function(event) {
     event.preventDefault();
-    startPlaybackGunit().then(function() {
-      console.log('The play() Promise fulfilled! Rock on!');
-    }).catch(function(error) {
-      console.log('The play() Promise rejected!');
-      console.log('Use the Play button instead.');
-      console.log(error);
-    })
     if (!!event.target.dataset.clear) {
       let clearForm = new MyForm()
       $('#portfolioForm').empty()
       $('#portfolioForm').append(clearForm.render())
       chart.unload()
-      startPlaybackSweedish().then(function() {
-        console.log('The play() Promise fulfilled! Rock on!');
-      }).catch(function(error) {
-        console.log('The play() Promise rejected!');
-        console.log('Use the Play button instead.');
-        console.log(error);
-      })
     }
     let selections = document.querySelectorAll('#portfolio')
     let chosen = []
@@ -141,10 +131,8 @@ class MyForm {
     return (
     `<form class="ui form">
       <div class="ui center aligned segment">
-        <audio id="gunit" src="./sounds/50_5.mp3" type="audio/mp3"></audio>
-        <audio id="sweedish" src="./sounds/dontworry.mp3" type="audio/mp3"></audio>
-        <button class="ui button" id="buttn" type="submit" data-create="create">Invest</button>
-        <button class="ui button" id="buttn" type="submit" data-clear="clear">Clear</button>
+        <button class="ui inverted green button" id="buttn" type="submit" data-create="create">Invest</button>
+        <button class="ui inverted grey button" id="buttn" type="submit" data-clear="clear">Clear</button>
       </div>
       <div class="ui centered cards">
         <div class="card">
@@ -247,26 +235,74 @@ class MyForm {
               </div>
           </div>
         </div>
+
+
+        <div class="card">
+          <div class="content">
+            <img class="right floated mini ui image" src="http://logodatabases.com/wp-content/uploads/2012/03/nvidia-logo-black-1024x772.jpg">
+            <div class="header">
+              NVIDIA
+            </div>
+            <div class="meta">
+              Ticker: NVDA
+            </div>
+            <div class="description">
+              Nvidia Corporation is an American technology company based in Santa Clara, California. It designs graphics processing units for the gaming and professional markets, as well as system on a chip units for the mobile computing and automotive market.</b>
+            </div>
+          </div>
+          <div class="extra content">
+              <div class="ui toggle checkbox">
+                <input type="checkbox" name="public" id="portfolio" value="NVIDIA">
+                <label>Invest?</label>
+              </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="content">
+            <img class="right floated mini ui image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IBM_logo.svg/2000px-IBM_logo.svg.png">
+            <div class="header">
+              IBM
+            </div>
+            <div class="meta">
+              Ticker: IBM
+            </div>
+            <div class="description">
+              IBM is an American multinational technology company headquartered in Armonk, New York, United States, with operations in over 170 countries.</b>
+            </div>
+          </div>
+          <div class="extra content">
+              <div class="ui toggle checkbox">
+                <input type="checkbox" name="public" id="portfolio" value="IBM">
+                <label>Invest?</label>
+              </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="content">
+            <img class="right floated mini ui image" src="http://logos-download.com/wp-content/uploads/2016/03/EBay_logo.png">
+            <div class="header">
+              eBay
+            </div>
+            <div class="meta">
+              Ticker: EBAY
+            </div>
+            <div class="description">
+              eBay Inc. is a multinational e-commerce corporation, facilitating online consumer-to-consumer and business-to-consumer sales. It is headquartered in San Jose, California.</b>
+            </div>
+          </div>
+          <div class="extra content">
+              <div class="ui toggle checkbox">
+                <input type="checkbox" name="public" id="portfolio" value="eBay">
+                <label>Invest?</label>
+              </div>
+          </div>
+        </div>
+
       </div>
     </form>`)
-    // `<form id="portForm">
-    //   <div>
-    //     <input type="checkbox" class="portfolio" name="subscribe" value="Apple">
-    //     <label for="subscribeNews">Apple</label>
-    //      <input type="checkbox" class="portfolio" name="subscribe" value="Amazon">
-    //     <label for="subscribeNews">Amazon</label>
-    //       <input type="checkbox" class="portfolio" name="subscribe" value="Google">
-    //     <label for="subscribeNews">Google</label>
-    //      <input type="checkbox" class="portfolio" name="subscribe" value="Facebook">
-    //     <label for="subscribeNews">Facebook</label>
-    //     <input type="checkbox" class="portfolio" name="subscribe" value="Microsoft">
-    //     <label for="subscribeNews">Microsoft</label>
-    //   </div>
-    //   <div>
-    //     <button type="submit" id="buttn" data-create="create">Create Portfolio</button>
-    //     <button type="submit" id="buttn" data-clear="clear">Clear</button>
-    //   </div>
-    // </form>`)
+
   }
 }
 
@@ -304,16 +340,23 @@ $.get('http://localhost:3000/stocks',function(data){
   let microsoft = filterData(data, 'Microsoft')
   let facebook = filterData(data, 'Facebook')
   let apple = filterData(data, 'Apple')
+  let nvidia = filterData(data, 'NVIDIA')
+  let ebay = filterData(data, 'eBay')
+  let ibm = filterData(data, 'IBM')
 
   // chart(chartArray)
   })
 
 }
 
-// function chart(dataPoint){
-
   var chart = c3.generate({
     bindto: '#chart',
+    padding: {
+        top: 20,
+        right: 100,
+        bottom: 20,
+        left: 100,
+    },
     data: {
       columns: [
 
@@ -334,7 +377,7 @@ $.get('http://localhost:3000/stocks',function(data){
       }
 
     }
+  
 
 });
 
-// }
